6a21d7672d9f61558747758c7aa8987d701ee4bb
Jeger <jegerq@gmail.com>
2019-03-17 08:53:47 +0200

    Added Cylinder class

---
+++ PLLUG/Cylinder.cs
@@ -0,0 +1,15 @@
+﻿namespace PLLUG
+{
+	public class Cylinder
+	{
+		public double Height { get; }
+		public double Diameter { get; }
+
+		public Cylinder(double height, double diameter)
+		{
+			Height = height;
+			Diameter = diameter;
+		}
+
+	}
+}
