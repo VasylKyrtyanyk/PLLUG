64460b8a34ca5b6d65dd132262334c64bcfcef2b
Jeger <jegerq@gmail.com>
2019-03-17 09:15:39 +0200

    Added method for  input data Sphere

--- PLLUG/HelperRead.cs
+++ PLLUG/HelperRead.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PLLUG.TypeObject;
 
 namespace PLLUG
 {
@@ -10,35 +11,50 @@ namespace PLLUG
 	{
 		public static Refrigerator InputDataRef()
 		{
-			Refrigerator _ref = new Refrigerator();
+			var _ref = new Refrigerator();
 
 			Console.Write("Enter size refrigerator:  Height =  ");
 			_ref.Height = Convert.ToDouble(Console.ReadLine());
+
 			Console.Write(" Width = ");
 			_ref.Width = Convert.ToDouble(Console.ReadLine());
+
 			Console.Write(" Length = ");
 			_ref.Length = Convert.ToDouble(Console.ReadLine());
+
 			return _ref;
 		}
 
 		public static Cylinder InputDataCyl()
 		{
-			Cylinder _cylinder = new Cylinder();
+			var cylinder = new Cylinder();
+
 			Console.Write("Enter size cylinder:  Height =  ");
-			_cylinder.Height = Convert.ToDouble(Console.ReadLine());
+			cylinder.Height = Convert.ToDouble(Console.ReadLine());
+
 			Console.Write(" Diameter = ");
-			_cylinder.Diameter = Convert.ToDouble(Console.ReadLine());
-			return _cylinder;
+			cylinder.Diameter = Convert.ToDouble(Console.ReadLine());
+
+			return cylinder;
+		}
+
+		public static Sphere InputDataSphere()
+		{
+			Console.WriteLine("Input diameter sphere ");
+			var diameter = Convert.ToDouble(Console.ReadLine());
+
+			return new Sphere(diameter);
 		}
 
 		public static ISubject InputDataDoor()
 		{
 			Console.WriteLine("Data Door");
-			double width, height;
+
 			Console.Write(" Width = ");
-			width = Convert.ToDouble(Console.ReadLine());
+			var width = Convert.ToDouble(Console.ReadLine());
+
 			Console.Write(" Height = ");
-			height = Convert.ToDouble(Console.ReadLine());
+			var height = Convert.ToDouble(Console.ReadLine());
 
 			return new SipmleDoor(width,height);
 		}
